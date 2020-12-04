3093ae9aa7a179e280bf7fde26b87ea3c7c2f0fa
장동빈 <qlsehd321@naver.com>
2020-12-04 09:49:38 +0900

    12

---
+++ src/test02/GuGuDan.java
@@ -0,0 +1,9 @@
+package test02;
+
+public class GuGuDan {
+	
+	public static void main (String[] args) {
+		
+	}
+
+}
---
+++ src/test02/Loop.java
@@ -0,0 +1,22 @@
+package test02;
+
+public class Loop {
+	
+	public static void main(String[] args) {
+		
+		for(int i=1; i<10; i++) {
+			if(i%2==1) {
+				System.out.println("i =" + i);			
+			}
+		}
+//		int a = 1;
+//		String str = "1";
+//		String sum = a + str;
+//		int num = 1;
+//		num = num + 1;
+//		num += 1;
+//		num++;
+//		System.out.println(++num);
+//		System.out.println(num);		
+	}
+}
